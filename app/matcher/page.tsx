108de53d85c868db0dcee8b2882868ec682cda8e
Miguel Santos <santosmiguel25@gmail.com>
2024-04-27 09:13:56 +0100

    Attempt to fix redirect on mobile

--- app/matcher/page.tsx
+++ app/matcher/page.tsx
@@ -13,10 +13,20 @@ export default async function MatcherPage() {
   return (
     <Layout>
       <div className="flex flex-col items-center justify-center py-2">
-        <div className="text-4xl font-black mb-10">Welcome, {user.name}!</div>
+        <div className="text-4xl font-black mb-10">Welcome, {user.firstName}!</div>
         <img src={user.picture} alt="Profile" className="w-40 h-40 rounded-full object-cover object-center mb-10" />
-        <div className="text-xl mb-10 text-primary justify-center font-medium">You have successfully completed your setup.</div>
+        <div className="text-lg mb-10 text-primary justify-center font-medium">You have successfully completed your setup.</div>
         <div className="text-xl mb-10 text-primary justify-center font-medium">Start matching now!</div>
+
+        <div className="text-primary justify-center">
+          <div className="justify-center mb-2 text-black font-bold">
+            Here's the data you provided:
+          </div>
+          <div className="text-lg">Origin: {user.origin.province}, {user.origin.subprovince}</div>
+          <div className="text-lg">Destination: {user.destination.province}, {user.destination.subprovince}</div>
+          <div className="text-lg">Major: {user.origin.major}</div>
+          <div className="text-lg">Contact: {user.contact.email}</div>
+        </div>
         <div className="flex flex-wrap justify-center">
         </div>
       </div>
--- components/setup-wizard/CompleteSetupComponent.tsx
+++ components/setup-wizard/CompleteSetupComponent.tsx
@@ -51,5 +51,5 @@ export default async function CompleteSetupComponent(formData: SetupFormData, us
   await updateUser(mappedUser);
 
   // Send the user to the /matcher page:
-  redirect('/matcher');
+  return redirect('/matcher');
 }
