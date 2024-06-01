58f1306ace314f2863d3b9024c24e8d15b69fdbf
Miguel Santos <santosmiguel25@gmail.com>
2024-06-01 19:24:34 +0100

    Improve beta chip positioning

--- components/NavComponent.tsx
+++ components/NavComponent.tsx
@@ -9,7 +9,7 @@ import { useEffect } from 'react';
 import { refreshLastLogin } from '@/lib/services/client/user.service';
 
 export default function Nav({ swapperUser }: { swapperUser: SwapperUser | null | undefined }) {
-  const earlyVersionChip = <span className='inline-block bg-tertiary text-primary font-bold text-base px-2 rounded-md shadow-xl select-none -translate-x-2'>Beta</span>;
+  const earlyVersionChip = <span className='inline-block bg-tertiary text-primary font-bold text-base px-2 rounded-md shadow-xl select-none ml-1 translate-y-3 -translate-x-2'>Beta</span>;
 
   useEffect(() => {
     if (swapperUser) {
@@ -21,14 +21,14 @@ export default function Nav({ swapperUser }: { swapperUser: SwapperUser | null |
     return (
       <nav className="p-4 bg-primary shadow-md">
         <div className="container mx-auto flex justify-between items-center">
-          <div>
-            <a href="/" className="text-4xl text-white font-bold mr-4 logo">{APP_NAME}</a>
+          <div className='w-3/6'>
+            <a href="/" className="text-4xl text-white font-bold logo">{APP_NAME}</a>
             {earlyVersionChip}
           </div>
-          <div>
+          <div className='flex gap-1'>
             <>
-              <Button href="/login" as={Link} className="mr-4 p-2 bg-tertiary rounded-lg text-md text-[#6D3AFA] font-medium shadow-md">Login</Button>
-              <Button href="/signup" as={Link} className="mr-4 p-2 bg-tertiary rounded-lg text-md text-[#6D3AFA] font-medium shadow-md">Sign Up</Button>
+              <Button href="/login" as={Link} className="p-2 bg-tertiary rounded-lg text-md text-[#6D3AFA] font-medium shadow-md">Login</Button>
+              <Button href="/signup" as={Link} className="p-2 bg-tertiary rounded-lg text-md text-[#6D3AFA] font-medium shadow-md">Sign Up</Button>
             </>
           </div>
         </div>
